a176203bd1d03e194a24c5beb96e44a6beb1f407
astrobyte21 <167555091+astrobyte21@users.noreply.github.com>
2024-10-29 21:21:23 +0545

    Update flower.js
    did some changes

--- flower.js
+++ flower.js
@@ -1,34 +1,32 @@
 const flowers = [
-  { name: "Aralia", pic: "/assets/1.jpg", price1: "Rs 250", price2: "" },
+  { name: "Aralia",
+   pic: "/assets/1.jpg",
+   price1: "Rs 250", 
+   price2: "" },
   {
     name: "Table Kamini",
     pic: "/assets/2.jpg",
     price1: "Small: Rs 250",
     price2: "Big: Rs 450",
   },
+ 
   {
-    name: "",
-    pic: "",
-    price1: "",
+    name: "Hibisucs",
+    pic: "/assets/3.jpg",
+    price1: "Small: Rs 250",
+    price2: "Big: Rs 550",
+  },
+  {
+    name: "Hibiscus",
+    pic: "/assets/4.jpg",
+    price1: "Rs 550",
     price2: "",
   },
   {
-    name: "",
-    pic: "",
-    price1: "",
-    price2: "",
-  },
-  {
-    name: "",
-    pic: "",
-    price1: "",
-    price2: "",
-  },
-  {
-    name: "",
-    pic: "",
-    price1: "",
-    price2: "",
+    name: "Rubber Plant(Balck)",
+    pic: "/assets/5.jpg",
+    price1: "Small: Rs 450",
+    price2: "Big:Rs 1050",
   },
   {
     name: "",
